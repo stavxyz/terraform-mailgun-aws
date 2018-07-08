441f75d0a17210cd5a8c29a003b042f3b3f1ede0
Sam Stavinoha <smlstvnh@gmail.com>
2018-07-08 15:49:08 -0700

    update comments in the module (todo: add to readme too)

--- main.tf
+++ main.tf
@@ -3,20 +3,42 @@
  *
  * This template creates the following resources
  *   - A Mailgun domain
- *   - An AWS Route53 Zone
+ *   - An AWS Route53 Zone (unless the zone_id variable is set)
  *   - AWS Route53 Records:
  *     - SPF, DKIM, CNAME, MX
  *
  * The MX records are optional, to disable set the variable mailgun_set_mx_for_inbound to false.
  *
- * If using an existing Route53 Zone, perform a terraform import on your zone:
+ * If using an existing Route53 Zone, you must set the 'zone_id' terraform variable for this module.
  *
- * $ terraform import module.my_instance.aws_route53_zone.this <your_route53_zone_id>
+ * Another option is to import your Route53 zone *into* this module, but that is probably not the
+ * "route" you want to go. (see what I did there?) But if you do:
  *
- * where the 'my_instance' portion is the name you choose:
+ * $ terraform import module.mailer.aws_route53_zone.this[0] <your_route53_zone_id>
  *
- * module "my_instance" {
+ * (The `[0]` is needed becauser it is a "conditional resource" and you must refer to the 'count'
+ *  index when importing, which is always [0])
+ *
+ * where the 'mailer' portion is the name you choose:
+ *
+ * module "mailer" {
  *   source = "github.com/samstav/terraform-mailgun-aws"
+ * }
+ *
+ * # In *either* case, you can refer to your zone id like this:
+ *
+ *   "${module.mailer.zone_id}"
+ *
+ * Since 'zone_id' is also an output variable from the terraform-mailgun-aws module.
+ *
+ * Here is an example of how one might use the 'zone_id' output variable:
+ *
+ * resource "aws_route53_record" "keybase_proof" {
+ *   zone_id = "${module.mailer.zone_id}"
+ *   name = "_keybase"
+ *   type = "TXT"
+ *   ttl = 300
+ *   records = ["keybase-site-verification=JHEIDMF-NkenDk389DnekD83ls8KLDjenf88slej89"]
  * }
  *
  */
