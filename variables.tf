99a2ed7088b7ec65e64ddb60f38622f39394b3f1
Sam Stavinoha <smlstvnh@gmail.com>
2018-07-08 15:30:48 -0700

    add zone_id input variable for existing zones

--- variables.tf
+++ variables.tf
@@ -22,6 +22,11 @@ variable "mailgun_set_mx_for_inbound" {
   description = "Affects terraform-mailgun-aws module behavior. Set to false or 0 to prevent this module from setting mailgun.org MX records on your Route53 Hosted Zone. See more information about how terraform handles booleans here: https://www.terraform.io/docs/configuration/variables.html"
 }
 
+variable "zone_id" {
+  default = 0
+  description = "You probbaly want to set this variable if 1) You are using an existing Route53 Zone 2) You don't want to delete and recreate that route53 zone and/or 3) You don't want to `terraform import` your existing Route53 Zone into this module. (Instructions for that are in the README). If this zone id is set, terrafrom-mailgun-aws will use this existing zone *instead of creating one for you*."
+}
+
 variable "record_ttl" {
   default = 300
   description = "Lifespan of DNS records"
