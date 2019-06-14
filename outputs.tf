625fb026c2f96da91acaea2cf0ec48dcca999488
Sam Stavinoha <smlstvnh@gmail.com>
2019-06-14 15:40:33 -0500

    backwards compat outputs

--- outputs.tf
+++ outputs.tf
@@ -26,3 +26,18 @@ output "cloudflare_zone_name" {
 output "cloudflare_zone_id" {
   value = "${element(concat(data.cloudflare_zones.selected.*.id, list("")), 0)}"
 }
+
+############################
+##### old aliases ##########
+############################
+
+output "zone_id" {
+  description = "DEPRECATED, USE PROVIDER SPECIFIC VALUE (unless we can figure out conditionals in the output value."
+  value = "${element(concat(data.aws_route53_zone.selected.*.zone_id, list("")), 0)}"
+}
+
+
+output "name_servers" {
+  description = "DEPRECATED, USE PROVIDER SPECIFIC VALUE (unless we can figure out conditionals in the output value."
+  value = "${element(concat(data.aws_route53_zone.selected.*.name_servers, list("")), 0)}"
+}
