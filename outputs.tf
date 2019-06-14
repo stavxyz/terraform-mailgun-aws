4f3bbf66708eb505067bf45bb7afa7247f32c7e6
Sam Stavinoha <smlstvnh@gmail.com>
2019-06-14 14:50:32 -0500

    update outputs

--- outputs.tf
+++ outputs.tf
@@ -6,11 +6,23 @@
  *   - name_servers
  */
 
-output "zone_id" {
-  value = "${data.aws_route53_zone.selected.zone_id}"
+# Be sure to check this output and set using the UpdateDomainNameservers API
+output "aws_name_servers" {
+  value = "${element(concat(data.aws_route53_zone.selected.*.name_servers, list("")), 0)}"
 }
 
-# Be sure to check this output and set using the UpdateDomainNameservers API
-output "name_servers" {
-  value = "${data.aws_route53_zone.selected.name_servers}"
+output "aws_zone_id" {
+  value = "${element(concat(data.aws_route53_zone.selected.*.zone_id, list("")), 0)}"
+}
+
+output "cloudflare_name_servers" {
+  value = "${element(concat(data.cloudflare_zone.selected.*.name_servers, list("")), 0)}"
+}
+
+output "cloudflare_zone_name" {
+  value = "${element(concat(data.cloudflare_zone.selected.*.name, list("")), 0)}"
+}
+
+output "cloudflare_zone_id" {
+  value = "${element(concat(data.cloudflare_zone.selected.*.id, list("")), 0)}"
 }
