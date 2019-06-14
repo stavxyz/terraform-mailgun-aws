294c40e77531da51552a4eeadfc0418418f10dad
Sam Stavinoha <smlstvnh@gmail.com>
2019-06-14 16:13:57 -0500

    trying to fix outputs

--- outputs.tf
+++ outputs.tf
@@ -8,24 +8,28 @@
 
 # Be sure to check this output and set using the UpdateDomainNameservers API
 output "aws_name_servers" {
-  value = "${element(concat(data.aws_route53_zone.selected.*.name_servers, list("")), 0)}"
+  value = "${element(data.aws_route53_zone.selected.*.name_servers, 0)}"
 }
 
 output "aws_zone_id" {
   value = "${element(concat(data.aws_route53_zone.selected.*.zone_id, list("")), 0)}"
 }
 
-output "cloudflare_name_servers" {
-  value = "${element(concat(data.cloudflare_zones.selected.*.name_servers, list("")), 0)}"
-}
-
 output "cloudflare_zone_name" {
-  value = "${element(concat(data.cloudflare_zones.selected.*.name, list("")), 0)}"
+  #value = "${element(concat(data.cloudflare_zones.selected.*.zones, list("")), 0)}"
+  value = "${element(data.cloudflare_zones.selected.*.zones, 0)}.name"
 }
 
 output "cloudflare_zone_id" {
-  value = "${element(concat(data.cloudflare_zones.selected.*.id, list("")), 0)}"
+  #value = "${element(concat(data.cloudflare_zones.selected.*.zones[0].id, list("")), 0)}"
+  value = "${element(data.cloudflare_zones.selected.*.zones, 0)}.id"
 }
+
+output "cloudflare_name_servers" {
+  #value = "${element(concat(data.cloudflare_zones.selected.*.zones, chunklist(list(""), 1)), 0)}"
+  value = "${element(data.cloudflare_zones.selected.*.zones, 0)}.name_servers"
+}
+
 
 ############################
 ##### old aliases ##########
@@ -38,6 +42,6 @@ output "zone_id" {
 
 
 output "name_servers" {
-  description = "DEPRECATED, USE PROVIDER SPECIFIC VALUE (unless we can figure out conditionals in the output value."
-  value = "${element(concat(data.aws_route53_zone.selected.*.name_servers, list("")), 0)}"
+  value = "${element(data.aws_route53_zone.selected.*.name_servers, 0)}"
 }
+
