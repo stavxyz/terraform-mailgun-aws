902feec7f8c12c2f762496042098f634aa7625ff
Sam Stavinoha <smlstvnh@gmail.com>
2019-08-13 17:53:33 -0500

    just some linting

--- variables.tf
+++ variables.tf
@@ -1,39 +1,41 @@
 variable "mailgun_smtp_password" {
-  type = "string"
+  type = string
 }
 
 variable "domain" {
-  type = "string"
+  type = string
 }
 
 variable "mailgun_spam_action" {
-  type = "string"
-  default = "tag"
+  type        = string
+  default     = "tag"
   description = "Spam filter behavior, (tag or disabled)."
 }
 
 variable "mailgun_wildcard" {
-  default = true
+  default     = true
   description = "Determines whether the domain will accept email for sub-domains."
 }
 
 variable "mailgun_set_mx_for_inbound" {
-  default = 1
+  default     = 1
   description = "Affects terraform-mailgun-aws module behavior. Set to false or 0 to prevent this module from setting mailgun.org MX records on your Route53 Hosted Zone. See more information about how terraform handles booleans here: https://www.terraform.io/docs/configuration/variables.html"
 }
 
 variable "zone_id" {
-  default = 0
+  type        = string
+  default     = "0"
   description = "You probaly want to set this variable if 1) You are using an existing Route53 Zone 2) You don't want to delete and recreate that route53 zone and/or 3) You don't want to `terraform import` your existing Route53 Zone into this module. (Instructions for that are in the README). If this zone id is set, terrafrom-mailgun-aws will use this existing zone *instead of creating one for you*."
 }
 
 variable "record_ttl" {
-  default = 300
+  default     = 300
   description = "Lifespan of DNS records"
 }
 
 variable "dns_provider" {
-  type = "string"
-  default = "aws"
+  type        = string
+  default     = "aws"
   description = "Set your DNS provider. Choices: 'aws' (default/for Route53), 'cloudflare' (for Cloudflare DNS)"
 }
+
