ed89bd2ef7dfb797d93d0e19eea2d821cefd6cd4
Sam Stavinoha <smlstvnh@gmail.com>
2019-08-13 17:56:36 -0500

    trying hard to get outputs to work + some 0.12 mods

--- outputs.tf
+++ outputs.tf
@@ -8,28 +8,40 @@
 
 # Be sure to check this output and set using the UpdateDomainNameservers API
 output "aws_name_servers" {
-  value = "${element(data.aws_route53_zone.selected.*.name_servers, 0)}"
+  value = data.aws_route53_zone.selected[0].name_servers
 }
 
 output "aws_zone_id" {
-  value = "${element(concat(data.aws_route53_zone.selected.*.zone_id, list("")), 0)}"
+  value = element(concat(data.aws_route53_zone.selected.*.zone_id, [""]), 0)
 }
 
 output "cloudflare_zone_name" {
-  #value = "${element(concat(data.cloudflare_zones.selected.*.zones, list("")), 0)}"
-  value = "${element(data.cloudflare_zones.selected.*.zones, 0)}.name"
+  value = length(data.cloudflare_zones.selected.*.zones) > 0 ? element(concat(data.cloudflare_zones.selected.*.zones, [""]), 0) : ""
 }
 
 output "cloudflare_zone_id" {
   #value = "${element(concat(data.cloudflare_zones.selected.*.zones[0].id, list("")), 0)}"
-  value = "${element(data.cloudflare_zones.selected.*.zones, 0)}.id"
+  #value = "${element(concat(data.cloudflare_zones.selected.*.zones, chunklist(list(""), 1)), 0)}.id"
+  value = flatten(
+    slice(
+      data.cloudflare_zones.selected.*.zones,
+      0,
+      length(data.cloudflare_zones.selected.*.zones) - max(length(data.cloudflare_zones.selected.*.zones) - 1, 0),
+    ),
+  )
 }
 
 output "cloudflare_name_servers" {
   #value = "${element(concat(data.cloudflare_zones.selected.*.zones, chunklist(list(""), 1)), 0)}"
-  value = "${element(data.cloudflare_zones.selected.*.zones, 0)}.name_servers"
+  #value = "${flatten(slice(data.cloudflare_zones.selected.*.zones, 0, 1))}[0].name_servers"
+  value = flatten(
+    slice(
+      data.cloudflare_zones.selected.*.zones,
+      0,
+      length(data.cloudflare_zones.selected.*.zones) - max(length(data.cloudflare_zones.selected.*.zones) - 1, 0),
+    ),
+  )
 }
-
 
 ############################
 ##### old aliases ##########
@@ -37,11 +49,10 @@ output "cloudflare_name_servers" {
 
 output "zone_id" {
   description = "DEPRECATED, USE PROVIDER SPECIFIC VALUE (unless we can figure out conditionals in the output value."
-  value = "${element(concat(data.aws_route53_zone.selected.*.zone_id, list("")), 0)}"
+  value       = element(concat(data.aws_route53_zone.selected.*.zone_id, [""]), 0)
 }
 
-
 output "name_servers" {
-  value = "${element(data.aws_route53_zone.selected.*.name_servers, 0)}"
+  value = flatten(data.aws_route53_zone.selected.*.name_servers)
 }
 
