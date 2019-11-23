3a33336fe41169c13ad2612dbea18b72dca61e79
Sam Stavinoha <smlstvnh@gmail.com>
2019-11-22 20:06:13 -0600

    0.12upgrade command

--- outputs.tf
+++ outputs.tf
@@ -7,10 +7,11 @@
  */
 
 output "zone_id" {
-  value = "${data.aws_route53_zone.selected.zone_id}"
+  value = data.aws_route53_zone.selected.zone_id
 }
 
 # Be sure to check this output and set using the UpdateDomainNameservers API
 output "name_servers" {
-  value = "${data.aws_route53_zone.selected.name_servers}"
+  value = data.aws_route53_zone.selected.name_servers
 }
+
